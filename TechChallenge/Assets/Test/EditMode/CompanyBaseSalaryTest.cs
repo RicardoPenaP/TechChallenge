bda51a0214e6b7057e77fa6cedfbfe7e617fbbac
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-02 20:20:51 -0300

    Removed unused using cases

--- TechChallenge/Assets/Test/EditMode/CompanyBaseSalaryTest.cs
+++ TechChallenge/Assets/Test/EditMode/CompanyBaseSalaryTest.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
-using NUnit.Framework;
-using UnityEngine;
-using UnityEngine.TestTools;
 using Company;
 using Company.Employees;
 using Company.Enums;
+using NUnit.Framework;
+using System.Collections.Generic;
 
 public class CompanyBaseSalaryTest
 {
