b285a4cd7ca6f9d63d529558e62867f9b622559a
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-02 18:56:38 -0300

    Created company section employees amount test class

---
+++ TechChallenge/Assets/Test/EditMode/CompanySectionEmployeesAmountTest.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using Company.Employees;
+using Company.Enums;
+
+public class CompanySectionEmployeesAmountTest
+{    
+    private int juniorsAmount;
+    private int semiSeniorsAmount;
+    private int seniorsAmount;
+
+    [Test]
+    public void HRSectionEmployeesTest()
+    {
+        
+    }
+
+   
+}
