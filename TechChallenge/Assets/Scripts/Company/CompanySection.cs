42ae65095f14a81c18cadb75f16a157407d7088c
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-02 18:39:42 -0300

    Refactorized code

--- TechChallenge/Assets/Scripts/Company/CompanySection.cs
+++ TechChallenge/Assets/Scripts/Company/CompanySection.cs
@@ -1,8 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Company.Interfaces;
-using Company.EmployeesInformation;
+
 namespace Company
 {
     public class CompanySection : MonoBehaviour
