9ef84638851d43f2e3cabc6d2606a4b59eb71b99
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-02 18:35:52 -0300

    Created company section class

---
+++ TechChallenge/Assets/Scripts/Company/CompanySection.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Company.Interfaces;
+using Company.EmployeesInformation;
+namespace Company
+{
+    public class CompanySection : MonoBehaviour
+    {       
+        
+    }
+}
