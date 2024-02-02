ff7c805580225023571be79a5443154cf36a993d
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-02 18:48:29 -0300

    Updated employees information class
    Removed class member

--- TechChallenge/Assets/Scripts/Company/Employees/EmployeesInformation.cs
+++ TechChallenge/Assets/Scripts/Company/Employees/EmployeesInformation.cs
@@ -3,22 +3,15 @@ using Company.Enums;
 namespace Company.Employees
 {
     public class EmployeesInformation 
-    {
-        private SeniorityLevels seniorityLevel;
+    {        
         private int employeesAmount;
-
-        public SeniorityLevels SeniorityLevels { get { return seniorityLevel; } }
+        
         public int EmployeesAmount { get { return employeesAmount; } }
 
         public EmployeesInformation()
         {
-            seniorityLevel = SeniorityLevels.None;
+           
             employeesAmount = 0;
-        }
-
-        public void SetSeniorityLevel(SeniorityLevels seniorityLevel)
-        {
-            this.seniorityLevel = seniorityLevel;
         }
 
         public void SetEmployeesAmount(int employeesAmount)
