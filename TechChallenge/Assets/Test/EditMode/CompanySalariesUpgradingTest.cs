1adfac4589bc235599c6a6d6124643301e4fa3ec
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-02 20:24:19 -0300

    Created new organization folder

--- TechChallenge/Assets/Test/EditMode/CompanySalariesUpgradingTest.cs
+++ TechChallenge/Assets/Test/EditMode/CompanySalariesUpgradingTest.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using Company;
+using Company.Employees;
+using Company.Enums;
 
 public class CompanySalariesUpgradingTest
 {
