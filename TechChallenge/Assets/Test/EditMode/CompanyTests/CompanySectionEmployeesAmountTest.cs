1b906f6f95d6b68d53097da086994438bc49ca48
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-04 23:48:17 -0300

    Updated company section employees amount test class
    Refactorized test function and added new method

--- TechChallenge/Assets/Test/EditMode/CompanyTests/CompanySectionEmployeesAmountTest.cs
+++ TechChallenge/Assets/Test/EditMode/CompanyTests/CompanySectionEmployeesAmountTest.cs
@@ -3,6 +3,7 @@ using Company.Employees;
 using Company.Enums;
 using NUnit.Framework;
 using System.Collections.Generic;
+using Company.Employees.EmployeesProperties;
 
 namespace EditMode.CompanyTests
 {
@@ -11,86 +12,209 @@ namespace EditMode.CompanyTests
         [Test]
         public void HRSectionEmployeesTest()
         {
-            Dictionary<SeniorityLevels, EmployeesInformation> sectionEmployees = new Dictionary<SeniorityLevels, EmployeesInformation>();
+            int[] employeesAmounts = new int[] { 5, 2, 13 };
 
-            sectionEmployees.Add(SeniorityLevels.Senior, new EmployeesInformation(5, 0, 0));
-            sectionEmployees.Add(SeniorityLevels.SemiSenior, new EmployeesInformation(2, 0, 0));
-            sectionEmployees.Add(SeniorityLevels.Junior, new EmployeesInformation(13, 0, 0));
+            SeniorityLevels[] seniorityLevels = new SeniorityLevels[] { SeniorityLevels.Senior, SeniorityLevels.SemiSenior, SeniorityLevels.Junior };
 
-            CompanySection companySection = new CompanySection();
-            companySection.SetSectionEmployeesDictionary(sectionEmployees);
+            int[] targetAmounts = GenerateCompanySectionEmployeesAmountArrayForTesting(employeesAmounts, seniorityLevels);
 
-            Assert.AreEqual(sectionEmployees, companySection.GetSectionEmployeesDictionary());
+            Assert.AreEqual(targetAmounts, employeesAmounts);
         }
 
         [Test]
         public void EngineeringSectionEmployeesTest()
         {
+            int[] employeesAmounts = new int[] { 5, 2, 13 };
             Dictionary<SeniorityLevels, EmployeesInformation> sectionEmployees = new Dictionary<SeniorityLevels, EmployeesInformation>();
 
-            sectionEmployees.Add(SeniorityLevels.Senior, new EmployeesInformation(50, 0, 0));
-            sectionEmployees.Add(SeniorityLevels.SemiSenior, new EmployeesInformation(68, 0, 0));
-            sectionEmployees.Add(SeniorityLevels.Junior, new EmployeesInformation(32, 0, 0));
+            EmployeesInformation seniorsInformation = new EmployeesInformation();
+            seniorsInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[0]));
+
+            EmployeesInformation semiSeniorsInformation = new EmployeesInformation();
+            semiSeniorsInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[1]));
+
+            EmployeesInformation juniorInformation = new EmployeesInformation();
+            juniorInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[2]));
+
+            sectionEmployees.Add(SeniorityLevels.Senior, seniorsInformation);
+            sectionEmployees.Add(SeniorityLevels.SemiSenior, semiSeniorsInformation);
+            sectionEmployees.Add(SeniorityLevels.Junior, juniorInformation);
 
             CompanySection companySection = new CompanySection();
             companySection.SetSectionEmployeesDictionary(sectionEmployees);
 
-            Assert.AreEqual(sectionEmployees, companySection.GetSectionEmployeesDictionary());
+            Dictionary<SeniorityLevels, EmployeesInformation> targetEmployees = companySection.GetSectionEmployeesDictionary();
+
+            int[] targetAmounts = new int[employeesAmounts.Length];
+
+            targetAmounts[0] = targetEmployees[SeniorityLevels.Senior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            targetAmounts[1] = targetEmployees[SeniorityLevels.SemiSenior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            targetAmounts[2] = targetEmployees[SeniorityLevels.Junior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+
+            Assert.AreEqual(targetAmounts, employeesAmounts);
         }
 
         [Test]
         public void ArtistSectionEmployeesTest()
         {
+            int[] employeesAmounts = new int[] { 5, 2, 13 };
             Dictionary<SeniorityLevels, EmployeesInformation> sectionEmployees = new Dictionary<SeniorityLevels, EmployeesInformation>();
 
-            sectionEmployees.Add(SeniorityLevels.Senior, new EmployeesInformation(5, 0, 0));
-            sectionEmployees.Add(SeniorityLevels.SemiSenior, new EmployeesInformation(20, 0, 0));
+            EmployeesInformation seniorsInformation = new EmployeesInformation();
+            seniorsInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[0]));
+
+            EmployeesInformation semiSeniorsInformation = new EmployeesInformation();
+            semiSeniorsInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[1]));
+
+            EmployeesInformation juniorInformation = new EmployeesInformation();
+            juniorInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[2]));
+
+            sectionEmployees.Add(SeniorityLevels.Senior, seniorsInformation);
+            sectionEmployees.Add(SeniorityLevels.SemiSenior, semiSeniorsInformation);
+            sectionEmployees.Add(SeniorityLevels.Junior, juniorInformation);
 
             CompanySection companySection = new CompanySection();
             companySection.SetSectionEmployeesDictionary(sectionEmployees);
 
-            Assert.AreEqual(sectionEmployees, companySection.GetSectionEmployeesDictionary());
+            Dictionary<SeniorityLevels, EmployeesInformation> targetEmployees = companySection.GetSectionEmployeesDictionary();
+
+            int[] targetAmounts = new int[employeesAmounts.Length];
+
+            targetAmounts[0] = targetEmployees[SeniorityLevels.Senior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            targetAmounts[1] = targetEmployees[SeniorityLevels.SemiSenior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            targetAmounts[2] = targetEmployees[SeniorityLevels.Junior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+
+            Assert.AreEqual(targetAmounts, employeesAmounts);
         }
 
         [Test]
         public void DesignSectionEmployeesTest()
         {
+            int[] employeesAmounts = new int[] { 5, 2, 13 };
             Dictionary<SeniorityLevels, EmployeesInformation> sectionEmployees = new Dictionary<SeniorityLevels, EmployeesInformation>();
 
-            sectionEmployees.Add(SeniorityLevels.Senior, new EmployeesInformation(10, 0, 0));
-            sectionEmployees.Add(SeniorityLevels.Junior, new EmployeesInformation(15, 0, 0));
+            EmployeesInformation seniorsInformation = new EmployeesInformation();
+            seniorsInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[0]));
+
+            EmployeesInformation semiSeniorsInformation = new EmployeesInformation();
+            semiSeniorsInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[1]));
+
+            EmployeesInformation juniorInformation = new EmployeesInformation();
+            juniorInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[2]));
+
+            sectionEmployees.Add(SeniorityLevels.Senior, seniorsInformation);
+            sectionEmployees.Add(SeniorityLevels.SemiSenior, semiSeniorsInformation);
+            sectionEmployees.Add(SeniorityLevels.Junior, juniorInformation);
 
             CompanySection companySection = new CompanySection();
             companySection.SetSectionEmployeesDictionary(sectionEmployees);
 
-            Assert.AreEqual(sectionEmployees, companySection.GetSectionEmployeesDictionary());
+            Dictionary<SeniorityLevels, EmployeesInformation> targetEmployees = companySection.GetSectionEmployeesDictionary();
+
+            int[] targetAmounts = new int[employeesAmounts.Length];
+
+            targetAmounts[0] = targetEmployees[SeniorityLevels.Senior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            targetAmounts[1] = targetEmployees[SeniorityLevels.SemiSenior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            targetAmounts[2] = targetEmployees[SeniorityLevels.Junior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+
+            Assert.AreEqual(targetAmounts, employeesAmounts);
         }
 
         [Test]
         public void PMsSectionEmployeesTest()
         {
+            int[] employeesAmounts = new int[] { 5, 2, 13 };
             Dictionary<SeniorityLevels, EmployeesInformation> sectionEmployees = new Dictionary<SeniorityLevels, EmployeesInformation>();
 
-            sectionEmployees.Add(SeniorityLevels.Senior, new EmployeesInformation(10, 0, 0));
-            sectionEmployees.Add(SeniorityLevels.SemiSenior, new EmployeesInformation(20, 0, 0));
+            EmployeesInformation seniorsInformation = new EmployeesInformation();
+            seniorsInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[0]));
+
+            EmployeesInformation semiSeniorsInformation = new EmployeesInformation();
+            semiSeniorsInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[1]));
+
+            EmployeesInformation juniorInformation = new EmployeesInformation();
+            juniorInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[2]));
+
+            sectionEmployees.Add(SeniorityLevels.Senior, seniorsInformation);
+            sectionEmployees.Add(SeniorityLevels.SemiSenior, semiSeniorsInformation);
+            sectionEmployees.Add(SeniorityLevels.Junior, juniorInformation);
 
             CompanySection companySection = new CompanySection();
             companySection.SetSectionEmployeesDictionary(sectionEmployees);
 
-            Assert.AreEqual(sectionEmployees, companySection.GetSectionEmployeesDictionary());
+            Dictionary<SeniorityLevels, EmployeesInformation> targetEmployees = companySection.GetSectionEmployeesDictionary();
+
+            int[] targetAmounts = new int[employeesAmounts.Length];
+
+            targetAmounts[0] = targetEmployees[SeniorityLevels.Senior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            targetAmounts[1] = targetEmployees[SeniorityLevels.SemiSenior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            targetAmounts[2] = targetEmployees[SeniorityLevels.Junior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+
+            Assert.AreEqual(targetAmounts, employeesAmounts);
         }
 
         [Test]
         public void CeoSectionEmployeesTest()
         {
+            int[] employeesAmounts = new int[] { 5, 2, 13 };
             Dictionary<SeniorityLevels, EmployeesInformation> sectionEmployees = new Dictionary<SeniorityLevels, EmployeesInformation>();
 
-            sectionEmployees.Add(SeniorityLevels.None, new EmployeesInformation(1, 0, 0));
+            EmployeesInformation seniorsInformation = new EmployeesInformation();
+            seniorsInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[0]));
+
+            EmployeesInformation semiSeniorsInformation = new EmployeesInformation();
+            semiSeniorsInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[1]));
+
+            EmployeesInformation juniorInformation = new EmployeesInformation();
+            juniorInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[2]));
+
+            sectionEmployees.Add(SeniorityLevels.Senior, seniorsInformation);
+            sectionEmployees.Add(SeniorityLevels.SemiSenior, semiSeniorsInformation);
+            sectionEmployees.Add(SeniorityLevels.Junior, juniorInformation);
 
             CompanySection companySection = new CompanySection();
             companySection.SetSectionEmployeesDictionary(sectionEmployees);
 
-            Assert.AreEqual(sectionEmployees, companySection.GetSectionEmployeesDictionary());
+            Dictionary<SeniorityLevels, EmployeesInformation> targetEmployees = companySection.GetSectionEmployeesDictionary();
+
+            int[] targetAmounts = new int[employeesAmounts.Length];
+
+            targetAmounts[0] = targetEmployees[SeniorityLevels.Senior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            targetAmounts[1] = targetEmployees[SeniorityLevels.SemiSenior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            targetAmounts[2] = targetEmployees[SeniorityLevels.Junior].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+
+            Assert.AreEqual(targetAmounts, employeesAmounts);
+        }
+
+        private int[] GenerateCompanySectionEmployeesAmountArrayForTesting(int[] startingValues, SeniorityLevels[] seniorityLevels)
+        {
+            int[] employeesAmounts = startingValues;
+            SeniorityLevels[] employeesSeniorityLevels = seniorityLevels;
+
+            int arraysLenght = employeesAmounts.Length;
+
+            Dictionary<SeniorityLevels, EmployeesInformation> sectionEmployees = new Dictionary<SeniorityLevels, EmployeesInformation>();
+
+            for (int i = 0; i < arraysLenght; i++)
+            {
+                EmployeesInformation employeesInformation = new EmployeesInformation();
+                employeesInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[i]));
+                sectionEmployees.Add(employeesSeniorityLevels[i], employeesInformation);
+            }
+
+            CompanySection companySection = new CompanySection();
+            companySection.SetSectionEmployeesDictionary(sectionEmployees);
+
+            Dictionary<SeniorityLevels, EmployeesInformation> targetEmployees = companySection.GetSectionEmployeesDictionary();
+
+            int[] targetAmounts = new int[employeesAmounts.Length];
+
+            for (int i = 0; i < arraysLenght; i++)
+            {
+                targetAmounts[i] = targetEmployees[employeesSeniorityLevels[i]].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            }
+
+            return targetAmounts;
         }
 
     }
