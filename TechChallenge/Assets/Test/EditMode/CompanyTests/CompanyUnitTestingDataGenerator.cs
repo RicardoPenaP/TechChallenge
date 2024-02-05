7c6a649766eec2c78a37a82404b0d73fcfb997df
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-04 23:49:44 -0300

    Created company unit testing data generator class

---
+++ TechChallenge/Assets/Test/EditMode/CompanyTests/CompanyUnitTestingDataGenerator.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EditMode.CompanyTests
+{
+    public static class CompanyUnitTestingDataGenerator
+    {
+        
+    }
+}
