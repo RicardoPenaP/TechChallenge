4be5507f6455ebd35bcf2c4e7e340a1bcb08089a
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-04 23:59:35 -0300

    Updated company unit testing data generator
    Added new method

--- TechChallenge/Assets/Test/EditMode/CompanyTests/CompanyUnitTestingDataGenerator.cs
+++ TechChallenge/Assets/Test/EditMode/CompanyTests/CompanyUnitTestingDataGenerator.cs
@@ -1,11 +1,42 @@
-using System.Collections;
+using Company;
+using Company.Employees;
+using Company.Employees.EmployeesProperties;
+using Company.Enums;
 using System.Collections.Generic;
-using UnityEngine;
 
 namespace EditMode.CompanyTests
 {
     public static class CompanyUnitTestingDataGenerator
     {
-        
+        public static int[] GenerateCompanySectionEmployeesAmountArrayForTesting(int[] startingValues, SeniorityLevels[] seniorityLevels)
+        {
+            int[] employeesAmounts = startingValues;
+            SeniorityLevels[] employeesSeniorityLevels = seniorityLevels;
+
+            int arraysLenght = employeesAmounts.Length;
+
+            Dictionary<SeniorityLevels, EmployeesInformation> sectionEmployees = new Dictionary<SeniorityLevels, EmployeesInformation>();
+
+            for (int i = 0; i < arraysLenght; i++)
+            {
+                EmployeesInformation employeesInformation = new EmployeesInformation();
+                employeesInformation.AddEmployeesProperty(new EmployeesAmount(employeesAmounts[i]));
+                sectionEmployees.Add(employeesSeniorityLevels[i], employeesInformation);
+            }
+
+            CompanySection companySection = new CompanySection();
+            companySection.SetSectionEmployeesDictionary(sectionEmployees);
+
+            Dictionary<SeniorityLevels, EmployeesInformation> targetEmployees = companySection.GetSectionEmployeesDictionary();
+
+            int[] targetAmounts = new int[employeesAmounts.Length];
+
+            for (int i = 0; i < arraysLenght; i++)
+            {
+                targetAmounts[i] = targetEmployees[employeesSeniorityLevels[i]].GetEmployeesProperty<EmployeesAmount>().ReadPropertyValue<int>();
+            }
+
+            return targetAmounts;
+        }
     }
 }
