933cabf550f1c668b5759a772fe49c398a2491b1
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-06 16:38:06 -0300

    Created company model class

---
+++ TechChallenge/Assets/Scripts/Company/CompanyMVC/CompanyModel/CompanyModel.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Company.CompanyMVC.CompanyModel
+{
+    public class CompanyModel : MonoBehaviour
+    {
+        
+    }
+}
