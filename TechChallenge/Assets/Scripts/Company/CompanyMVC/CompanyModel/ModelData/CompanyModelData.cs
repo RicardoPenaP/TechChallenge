6ad9d1329a125b623c2661d6366aec95a3f5a76f
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-06 16:44:11 -0300

    Created company model data class

---
+++ TechChallenge/Assets/Scripts/Company/CompanyMVC/CompanyModel/ModelData/CompanyModelData.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Company.CompanyMVC.CompanyModel.ModelData
+{
+    [CreateAssetMenu(fileName = "NewCompanyModelData", menuName = "Company Model Data")]
+    public class CompanyModelData : ScriptableObject
+    {
+        [Header("Company Model Data")]
+        [SerializeField] private float value;
+    }
+}
