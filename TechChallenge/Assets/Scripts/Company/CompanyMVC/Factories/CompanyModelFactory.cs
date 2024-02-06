33146f57e6aaf2b09c058cef9e8bec21f7800b70
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-06 17:32:34 -0300

    Updated company model factory class
    Added new creation method

--- TechChallenge/Assets/Scripts/Company/CompanyMVC/Factories/CompanyModelFactory.cs
+++ TechChallenge/Assets/Scripts/Company/CompanyMVC/Factories/CompanyModelFactory.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Company.CompanyMVC.CompanyModel.ModelData;
+using Company.Employees;
 using Company;
 
 namespace Company.CompanyMVC.Factories
@@ -11,6 +12,20 @@ namespace Company.CompanyMVC.Factories
         [Header("Company Model Factory")]
         [SerializeField] private CompanyModelData techChallengeCompanyModelData;
 
-        public  CreateNewCompany
+        public CompanyInformation CreateNewCompany()
+        {
+            List<CompanySection> companySections = new List<CompanySection>();
+
+            for (int i = 0; i < techChallengeCompanyModelData.CompanySections.Length; i++)
+            {
+                //companySections[i] = new CompanySection();
+                //List<EmployeesProperty> employeesPropertiesList = new List<EmployeesProperty>();
+                
+
+                //companySections[i].SetEmployeesInformationList();
+            }
+
+            return new CompanyInformation(techChallengeCompanyModelData.CompanyName, companySections);
+        }
     }
 }
