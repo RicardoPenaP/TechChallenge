4c7d77229e31636e9be958ce20c8f230427db6ca
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-06 13:00:02 -0300

    Updated company assembly definition file
    Added new assembly definition files references

--- TechChallenge/Assets/Scripts/Company/CompanyMVC/CompanyView/InformationPanel/SectionInformationPropertiesView.cs
+++ TechChallenge/Assets/Scripts/Company/CompanyMVC/CompanyView/InformationPanel/SectionInformationPropertiesView.cs
@@ -2,20 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace Company
+
+namespace Company.CompanyMVC.CompanyView.InformationPanel
 {
     public class SectionInformationPropertiesView : MonoBehaviour
     {
-        // Start is called before the first frame update
-        void Start()
-        {
-        
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-        
-        }
+        //[Header("Section Information Properties View")]
+        //[SerializeField] private
     }
 }
