697ba0bb486140256f4d033b5ca5a167a2a0256d
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-06 13:13:28 -0300

    Updated section information properties view class
    Added methods to set and clear the properties text

--- TechChallenge/Assets/Scripts/Company/CompanyMVC/CompanyView/InformationPanel/SectionInformationPropertiesView.cs
+++ TechChallenge/Assets/Scripts/Company/CompanyMVC/CompanyView/InformationPanel/SectionInformationPropertiesView.cs
@@ -1,13 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 
 namespace Company.CompanyMVC.CompanyView.InformationPanel
 {
     public class SectionInformationPropertiesView : MonoBehaviour
     {
-        //[Header("Section Information Properties View")]
-        //[SerializeField] private
+        [Header("Section Information Properties View")]
+        [SerializeField] private TextMeshProUGUI sectionNameText;
+        [SerializeField] private TextMeshProUGUI seniorityLevelText;
+        [SerializeField] private TextMeshProUGUI employeesAmountText;
+        [SerializeField] private TextMeshProUGUI salaryIncrementPercentageText;
+        [SerializeField] private TextMeshProUGUI baseSalaryText;
+        [SerializeField] private TextMeshProUGUI actualSalaryText;
+
+        public void ClearAllPropertiesText()
+        {
+            sectionNameText.text = " ";
+            seniorityLevelText.text = " ";
+            employeesAmountText.text = " ";
+            salaryIncrementPercentageText.text = " ";
+            baseSalaryText.text = " ";
+            actualSalaryText.text = " ";
+        }
+
+        public void SetAllPropertiesText(string sectionNameTex, string seniorityLevelText, string employeesAmountText, string salaryIncrementPercentageText, string baseSalaryText, string actualSalaryText)
+        {
+            this.sectionNameText.text = sectionNameTex;
+            this.seniorityLevelText.text = seniorityLevelText;
+            this.employeesAmountText.text = employeesAmountText;
+            this.salaryIncrementPercentageText.text = salaryIncrementPercentageText;
+            this.baseSalaryText.text = baseSalaryText;
+            this.actualSalaryText.text = actualSalaryText;
+        }
+
     }
 }
