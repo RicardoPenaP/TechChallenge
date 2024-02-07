166ef2d8aa92efab0202cafa2b503886b0ab5353
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-07 00:41:59 -0300

    Updated model data formatter class
    Added static formatter methods

--- TechChallenge/Assets/Scripts/CompanyMVC/DataFormaters/ModelDataFormatter.cs
+++ TechChallenge/Assets/Scripts/CompanyMVC/DataFormaters/ModelDataFormatter.cs
@@ -1,4 +1,8 @@
-using System.Collections;
+using Company;
+using Company.Employees;
+using Company.Employees.EmployeesProperties;
+using Company.Enums;
+using CompanyMVC.CompanyModel.ModelData;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,6 +10,34 @@ namespace Company.DataFormatters
 {
     public static class ModelDataFormatter 
     {
-        
+        public static List<EmployeesInformation> FormatCompanySectionDataToEmployeesInformationList(CompanySectionData companySectionData)
+        {
+            List<EmployeesInformation> employeesInformationList = new List<EmployeesInformation>();
+
+            for (int i = 0; i < companySectionData.SeniorityLevels.Length; i++)
+            {
+                List<EmployeesProperty> employeesProperties = CreateEmployeesPropertiesList(
+                    companySectionData.SeniorityLevels[i], companySectionData.EmployeesAmount[i],
+                    companySectionData.SalaryIncrementPercentage[i], companySectionData.BaseSalary[i]);
+
+                EmployeesInformation newEmployeesInformation = new EmployeesInformation();
+                newEmployeesInformation.SetEmployeesPropeties(employeesProperties);
+                employeesInformationList.Add(newEmployeesInformation);
+            }
+            return employeesInformationList;
+        }
+
+        private static List<EmployeesProperty> CreateEmployeesPropertiesList(SeniorityLevels seniorityLevel, int employeesAmount, float salaryIncrementPercentage, float baseSalary)
+        {
+            List<EmployeesProperty> employeesProperties = new List<EmployeesProperty>();
+            employeesProperties.Add(new EmployeesSeniorityLevel(seniorityLevel));
+            employeesProperties.Add(new EmployeesAmount(employeesAmount));
+            employeesProperties.Add(new SalaryIncrementPercentage(salaryIncrementPercentage));
+            employeesProperties.Add(new BaseSalary(baseSalary));
+            employeesProperties.Add(new ActualSalary(baseSalary));
+
+            return employeesProperties;
+        }
+
     }
 }
--- TechChallenge/Assets/Scripts/CompanyMVC/Factories/CompanyModelFactory.cs
+++ TechChallenge/Assets/Scripts/CompanyMVC/Factories/CompanyModelFactory.cs
@@ -6,6 +6,7 @@ using CompanyMVC.CompanyModel.ModelData;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace CompanyMVC.Factories
 {
     public class CompanyModelFactory : MonoBehaviour
