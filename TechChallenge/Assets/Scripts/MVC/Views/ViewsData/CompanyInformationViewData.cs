386d72528af4c0e6130d152790c4bb47b6831fbc
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-07 13:14:20 -0300

    Updated company information view data class
    Added company name property

--- TechChallenge/Assets/Scripts/MVC/Views/ViewsData/CompanyInformationViewData.cs
+++ TechChallenge/Assets/Scripts/MVC/Views/ViewsData/CompanyInformationViewData.cs
@@ -2,6 +2,8 @@ namespace MVC.Views.ViewsData
 {
     public class CompanyInformationViewData
     {
+        private string companyName;
+        public string CompanyName => companyName;
         private SectionInformationViewData[] companySectionsInformationViewDatas;
 
         public SectionInformationViewData[] CompanySectionsInformationViewDatas => companySectionsInformationViewDatas;
