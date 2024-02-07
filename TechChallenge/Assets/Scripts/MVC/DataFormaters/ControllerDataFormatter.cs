a3f12b8588eb6a152844e1d5925353eaa567fde3
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-07 13:20:03 -0300

    Updated controller data formatter class
    Updated format method

--- TechChallenge/Assets/Scripts/MVC/DataFormaters/ControllerDataFormatter.cs
+++ TechChallenge/Assets/Scripts/MVC/DataFormaters/ControllerDataFormatter.cs
@@ -1,5 +1,6 @@
 using Company;
 using MVC.Views.ViewsData;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace MVC.DataFormatters
@@ -8,6 +9,16 @@ namespace MVC.DataFormatters
     {
         public static CompanyInformationViewData FormatCompanyInformationToCompanyInformationViewData(CompanyInformation companyInformation)
         {
+            List<CompanySection> companySections = companyInformation.GetCompanySectionsList();
+            int companySectionsAmount = companySections.Count;
+
+            SectionInformationViewData[] sectionInformationViewDataArray = new SectionInformationViewData[companySectionsAmount];
+            for (int i = 0; i < sectionInformationViewDataArray.Length; i++)
+            {
+                string sectionName = companySections[i].GetSectionName();
+                
+            }
+
 
             return null;
         }
