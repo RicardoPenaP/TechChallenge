600f3cf2824686dfe8e91d9db499721be536a6f3
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-07 00:30:50 -0300

    Updated company model class
    Added initialization method and class members

--- TechChallenge/Assets/Scripts/CompanyMVC/CompanyModel/CompanyModel.cs
+++ TechChallenge/Assets/Scripts/CompanyMVC/CompanyModel/CompanyModel.cs
@@ -1,9 +1,22 @@
 using UnityEngine;
+using CompanyMVC.Factories;
+using Company;
 
 namespace CompanyMVC.CompanyModel
 {
     public class CompanyModel : MonoBehaviour
     {
-        
+        [Header("Company Model")]
+        [Header("Runtime References")]
+        [SerializeField] private CompanyModelFactory companyModelFactory;
+
+        private CompanyInformation companyInformation;
+
+        private void Awake()
+        {
+            companyInformation = companyModelFactory.CreateNewCompany();
+        }
+
+
     }
 }
