c236f80128e44a8e983711d78f970fa942392d70
RicardoPenaP <113145639+RicardoPenaP@users.noreply.github.com>
2024-02-07 13:38:37 -0300

    Updated controller data formatter class
    Updated format method logic

--- TechChallenge/Assets/Scripts/MVC/DataFormaters/ControllerDataFormatter.cs
+++ TechChallenge/Assets/Scripts/MVC/DataFormaters/ControllerDataFormatter.cs
@@ -1,11 +1,13 @@
 using Company;
+using Company.Employees;
 using MVC.Views.ViewsData;
 using System.Collections.Generic;
+using Company.Employees.EmployeesProperties;
 using UnityEngine;
 
 namespace MVC.DataFormatters
 {
-    public static class ControllerDataFormatter 
+    public static class ControllerDataFormatter
     {
         public static CompanyInformationViewData FormatCompanyInformationToCompanyInformationViewData(CompanyInformation companyInformation)
         {
@@ -13,14 +15,39 @@ namespace MVC.DataFormatters
             int companySectionsAmount = companySections.Count;
 
             SectionInformationViewData[] sectionInformationViewDataArray = new SectionInformationViewData[companySectionsAmount];
-            for (int i = 0; i < sectionInformationViewDataArray.Length; i++)
+
+
+            for (int i = 0; i < companySectionsAmount; i++)
             {
                 string sectionName = companySections[i].GetSectionName();
-                
+                List<EmployeesInformation> employeesInformation = companySections[i].GetEmployeesInformationList();
+                int propertiesAmount = employeesInformation.Count;
+
+                string[] seniorityLevelsArray = new string[propertiesAmount];
+                string[] employeesAmountsArray = new string[propertiesAmount];
+                string[] salaryIncrementPercentagesArray = new string[propertiesAmount];
+                string[] baseSalariesArray = new string[propertiesAmount];
+                string[] actualSalariesArray = new string[propertiesAmount];
+
+                for (int j = 0; j < propertiesAmount; j++)
+                {
+                    seniorityLevelsArray[j] = employeesInformation[i].GetEmployeesProperty<EmployeesSeniorityLevel>().ToString();
+                    employeesAmountsArray[j] = employeesInformation[i].GetEmployeesProperty<EmployeesAmount>().ToString();
+                    salaryIncrementPercentagesArray[j] = employeesInformation[i].GetEmployeesProperty<SalaryIncrementPercentage>().ToString();
+                    baseSalariesArray[j] = employeesInformation[i].GetEmployeesProperty<BaseSalary>().ToString();
+                    actualSalariesArray[j] = employeesInformation[i].GetEmployeesProperty<ActualSalary>().ToString();
+                }
+
+                SectionInformationViewData newSectionInformationViewData = new SectionInformationViewData(sectionName,
+                                                                               seniorityLevelsArray, employeesAmountsArray,
+                                                                               salaryIncrementPercentagesArray, baseSalariesArray,
+                                                                               actualSalariesArray);
+
+
+                sectionInformationViewDataArray[i] = newSectionInformationViewData;
             }
 
-
-            return null;
+            return new CompanyInformationViewData(companyInformation.CompanyName, sectionInformationViewDataArray);
         }
     }
 }
